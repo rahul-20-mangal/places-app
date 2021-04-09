cf065e26a6734aaf2913206b0b926d2645251172
Rahul Mangal <rahul200826101998@gmail.com>
2021-04-09 19:42:13 +0530

    Update model

--- spot/models.py
+++ spot/models.py
@@ -1,13 +1,21 @@
 from django.db import models
 from django.contrib.gis.db.models import PointField
+from django.urls import reverse
 # Create your models here.
 
 class Place(models.Model):
-    title = models.CharFiled(max_length=200)
-    location = PointField(null=True)
-    description = models.TextField(max_length=2000)
-    address = models.CharFiled(max_length=200)
-    phone = models.IntegerField()
-    city = models.CharFiled(max_length=200)
-    type_of_place = models.CharFiled(max_length=200)
-    tags = models.CharFiled(max_length=200)
+    title = models.CharField(max_length=200, null=True)
+    location = PointField(null=True, blank=True)
+    description = models.TextField(max_length=2000, null=True)
+    address = models.CharField(max_length=200, null=True)
+    phone = models.IntegerField(null=True)
+    city = models.CharField(max_length=200, null=True)
+    type_of_place = models.CharField(max_length=200, null=True)
+    tags = models.CharField(max_length=200, null=True)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        """Returns the url to access a particular place."""
+        return reverse('detail-place', args=[str(self.id)])
