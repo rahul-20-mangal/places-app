98013dd82d62326f7db7d66a3f3420554ea7b433
Rahul Mangal <rahul200826101998@gmail.com>
2021-04-09 17:09:10 +0530

    Create place model

--- spot/models.py
+++ spot/models.py
@@ -3,4 +3,11 @@ from django.contrib.gis.db.models import PointField
 # Create your models here.
 
 class Place(models.Model):
+    title = models.CharFiled(max_length=200)
     location = PointField(null=True)
+    description = models.TextField(max_length=2000)
+    address = models.CharFiled(max_length=200)
+    phone = models.IntegerField()
+    city = models.CharFiled(max_length=200)
+    type_of_place = models.CharFiled(max_length=200)
+    tags = models.CharFiled(max_length=200)
