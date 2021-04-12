e6b89d1cda24f14335c413afa10b7106a33b1f39
Rahul Mangal <rahul200826101998@gmail.com>
2021-04-12 16:27:21 +0530

    Update settings

--- placesapp/settings.py
+++ placesapp/settings.py
@@ -77,6 +77,17 @@ WSGI_APPLICATION = 'placesapp.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.contrib.gis.db.backends.postgis',
+#         'NAME': 'my_db',
+#         'USER': 'admin',
+#         'PASSWORD': 'mangal2008',
+#         'HOST': '127.0.0.1',
+#         'PORT': '5432',
+#     }
+# }
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.contrib.gis.db.backends.postgis',
